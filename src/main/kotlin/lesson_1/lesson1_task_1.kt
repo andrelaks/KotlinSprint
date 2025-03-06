31a15fc832cc690bdf5f035d9568e18bf1de0064
andrelaks <alexkar86@gmail.com>
2025-03-06 19:34:41 +0300

    KS-1-1 Первое задание

---
+++ src/main/kotlin/lesson_1/lesson1_task_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main(){
+
+    val numberOfOrders = 75
+    val byeMessage = "Thank you for your order"
+
+    println(numberOfOrders)
+    println(byeMessage)
+
+}
